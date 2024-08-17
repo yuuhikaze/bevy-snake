5477a159c35ef05e219c6187f1f51a1e118816b4
friendlyPotat0 <friendlyPotat0@protonmail.com>
2024-08-17 18:00:30 -0500

    feat: grid added to constrain game elements

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,46 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 const SNAKE_HEAD_COLOR: Color = Color::srgb(0.7, 0.7, 0.7);
+const ARENA_WIDTH: u32 = 10;
+const ARENA_HEIGHT: u32 = 10;
+
+#[derive(Component, Clone, Copy, PartialEq, Eq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Size {
+    width: f32,
+    height: f32,
+}
+
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
 
 #[derive(Component)]
 struct SnakeHead;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: (500., 500.).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .insert_resource(ClearColor(Color::srgb(0.04, 0.04, 0.04)))
         .add_systems(Startup, (setup_camera, spawn_snake).chain())
         .add_systems(Update, snake_movement)
+        .add_systems(PostUpdate, (position_translation, size_scaling))
         .run();
 }
 
@@ -30,25 +61,60 @@ fn spawn_snake(mut commands: Commands) {
             },
             ..default()
         })
-        .insert(SnakeHead);
+        .insert(SnakeHead)
+        .insert(Position { x: 3, y: 3 })
+        .insert(Size::square(0.8));
+}
+
+fn size_scaling(
+    windows: Query<&Window, With<PrimaryWindow>>,
+    mut query: Query<(&Size, &mut Transform)>,
+) {
+    let window = windows.get_single().unwrap();
+    for (sprite_size, mut transform) in query.iter_mut() {
+        transform.scale = Vec3::new(
+            sprite_size.width / ARENA_WIDTH as f32 * window.width() as f32,
+            sprite_size.height / ARENA_HEIGHT as f32 * window.height() as f32,
+            1.0,
+        );
+    }
+}
+
+fn position_translation(windows: Query<&Window>, mut query: Query<(&Position, &mut Transform)>) {
+    fn convert(position: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        position / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+    }
+    let window = windows.get_single().unwrap();
+    for (position, mut transform) in query.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(position.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(
+                position.y as f32,
+                window.height() as f32,
+                ARENA_HEIGHT as f32,
+            ),
+            0.0,
+        );
+    }
 }
 
 fn snake_movement(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut head_positions: Query<&mut Transform, With<SnakeHead>>,
+    mut head_positions: Query<&mut Position, With<SnakeHead>>,
 ) {
-    head_positions.iter_mut().for_each(|mut transform| {
+    head_positions.iter_mut().for_each(|mut position| {
         if keyboard_input.pressed(KeyCode::ArrowUp) || keyboard_input.pressed(KeyCode::KeyW) {
-            transform.translation.y += 2.;
+            position.y += 2;
         }
         if keyboard_input.pressed(KeyCode::ArrowDown) || keyboard_input.pressed(KeyCode::KeyS) {
-            transform.translation.y -= 2.;
+            position.y -= 2;
         }
         if keyboard_input.pressed(KeyCode::ArrowRight) || keyboard_input.pressed(KeyCode::KeyD) {
-            transform.translation.x += 2.;
+            position.x += 2;
         }
         if keyboard_input.pressed(KeyCode::ArrowLeft) || keyboard_input.pressed(KeyCode::KeyA) {
-            transform.translation.x -= 2.;
+            position.x -= 2;
         }
     });
 }
